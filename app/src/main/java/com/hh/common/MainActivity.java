c390f09448e8e71b670ae14f576482dcc98d1604
huihui <huihui.zhu@yingzt.com>
2017-11-03 19:37:56 +0800

    dialog

--- app/src/main/java/com/hh/common/MainActivity.java
+++ app/src/main/java/com/hh/common/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hh.common;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.hh.common.dialog.CustomDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final CustomDialog.Builder builder = new CustomDialog.Builder(this);
+
+
+        builder
+//                .setTitle("前三期不能提前还")
+                .setMessage("现在处在第三期，11月3号把第三期还了之后")
+                .setCancelButton("取消", null)
+                .setConfirmButton("确定", null)
+        .setCanceledOnTouchOutside(false)
+                ;
+        builder.show();
+
+        findViewById(R.id.open).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder.show();
+            }
+        });
     }
 }
