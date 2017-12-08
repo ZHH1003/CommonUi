ba5e70164f38805d0568a49123bedf7f871430b0
huihui <huihui100304216@163.com>
2017-12-08 10:34:46 +0800

    底部弹窗dialog

--- app/src/main/java/com/hh/common/MainActivity.java
+++ app/src/main/java/com/hh/common/MainActivity.java
@@ -5,8 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.Spanned;
 import android.view.View;
+import android.widget.Toast;
 
+import com.hh.common.dialog.BottomMenuDialog;
 import com.hh.common.dialog.CustomDialog;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ArrayList<CharSequence> list = new ArrayList<>();
+        list.add(Html.fromHtml(getResources().getString(R.string.menu)));
+        list.add("从手机相册选择");
+        list.add("从空间相册选择");
+        new BottomMenuDialog.Builder(MainActivity.this)
+                .setMenus(list)
+                .setOnItemClickListener(new BottomMenuDialog.OnItemClickListener() {
+                    @Override
+                    public void click(View view, CharSequence menu) {
+                        Toast.makeText(MainActivity.this, menu, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
 
 
         final Spanned spanned = Html.fromHtml(getResources().getString(R.string.button_text));
@@ -44,9 +63,23 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.textView2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CustomDialog.Builder builder = new CustomDialog.Builder(MainActivity.this);
-                builder.setMessage("亲爱的用户,你还没有借款记录,需要资金周转记得找卡贷君哈!")
-                        .setSingleButton("取消", null)
+//                CustomDialog.Builder builder = new CustomDialog.Builder(MainActivity.this);
+//                builder.setMessage("亲爱的用户,你还没有借款记录,需要资金周转记得找卡贷君哈!")
+//                        .setSingleButton("取消", null)
+//                        .show();
+                ArrayList<CharSequence> list = new ArrayList<>();
+                list.add(Html.fromHtml(getResources().getString(R.string.menu)));
+                list.add("从手机相册选择");
+                list.add("从空间相册选择");
+                new BottomMenuDialog.Builder(MainActivity.this)
+                        .setTitle("你可以将照片上传至照片墙")
+                        .setMenus(list)
+                        .setOnItemClickListener(new BottomMenuDialog.OnItemClickListener() {
+                            @Override
+                            public void click(View view, CharSequence menu) {
+                                Toast.makeText(MainActivity.this, menu, Toast.LENGTH_SHORT).show();
+                            }
+                        })
                         .show();
             }
         });
